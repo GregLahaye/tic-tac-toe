fff2ff99806025d96cc1a1b2ea02195f6f6188d4
Greg Lahaye <greg@lahayes.net>
2021-07-06 13:47:44 +0800

    add logging

--- backend/cache.go
+++ backend/cache.go
@@ -5,14 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
 func GetCache(hash string) (response Response, exists bool) {
-	url := fmt.Sprintf("https://kvdb.io/9zfvdU7e32RxRyMStNm2zP/%s", hash)
+	url := fmt.Sprintf("https://kvdb.io/7XHQsAyLHuFZFNSWYksVGC/%s", hash)
+	log.Printf("[%s] Getting %s", hash, url)
 
 	res, err := http.Get(url)
 	if res.StatusCode == 404 {
+		log.Printf("[%s] Not Found", hash)
 		return
 	}
 	if err != nil {
@@ -27,18 +30,26 @@ func GetCache(hash string) (response Response, exists bool) {
 
 	json.Unmarshal(content, &response)
 
+	log.Printf("[%s] Got %v", hash, response)
+
 	return response, true
 }
 
 func SetCache(hash string, response Response) error {
-	url := fmt.Sprintf("https://kvdb.io/9zfvdU7e32RxRyMStNm2zP/%s", hash)
+	url := fmt.Sprintf("https://kvdb.io/7XHQsAyLHuFZFNSWYksVGC/%s", hash)
+	log.Printf("Setting %s", url)
 
 	content, err := json.Marshal(response)
 	if err != nil {
+		log.Printf("%v", err)
 		return err
 	}
 
-	_, err = http.Post(url, "application/json", bytes.NewBuffer(content))
+	if _, err = http.Post(url, "application/json", bytes.NewBuffer(content)); err != nil {
+		log.Printf("%v", err)
+	}
+
+	log.Printf("[%s] Set %v", hash, response)
 
 	return err
 }
